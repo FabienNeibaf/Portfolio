54cd547b8ef291b1d3e259077d039a002fb387e8
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-12-11 10:08:16 +0300

    Update projects' list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import tada from '../images/Tada.png';
+import Github from '../images/github.svg';
 import weather from '../images/Weather.png';
+import dashboard from '../images/Dashboard.png';
 import tictactoe from '../images/TicTacToe.png';
 import battleship from '../images/Battleship.png';
 import earthdiary from '../images/Earthdiary.png';
@@ -10,35 +12,51 @@ const list = [
   {
     id: 1,
     img: battleship,
-    url: 'https://fabienneibaf.github.io/Battleship/',
+    demo: 'https://fabienneibaf.github.io/Battleship/',
+    github: 'https://github.com/FabienNeibaf/Battleship',
   },
   {
     id: 2,
     img: earthdiary,
-    url: 'https://earthdiary.herokuapp.com/',
+    demo: 'https://earthdiary.herokuapp.com/',
+    github: 'https://github.com/maelfosso/microverse_ror_final_project',
   },
   {
     id: 3,
     img: tada,
-    url: 'https://fabienneibaf.github.io/TodoList/',
+    demo: 'https://fabienneibaf.github.io/TodoList/',
+    github: 'https://github.com/FabienNeibaf/TodoList',
   },
   {
     id: 4,
     img: tictactoe,
-    url: 'https://fabienneibaf.github.io/TicTacToe/',
+    demo: 'https://fabienneibaf.github.io/TicTacToe/',
+    github: 'https://github.com/FabienNeibaf/TicTacToe',
   },
   {
     id: 5,
     img: weather,
-    url: 'https://fabienneibaf.github.io/Weather-App/',
+    demo: 'https://fabienneibaf.github.io/Weather-App/',
+    github: 'https://github.com/FabienNeibaf/Weather-App',
+  },
+  {
+    id: 6,
+    img: dashboard,
+    demo: 'https://fabienneibaf.github.io/Dashboard/',
+    github: 'https://github.com/FabienNeibaf/Dashboard',
   },
 ];
 
 const Item = ({ item }) => (
   <li className="item">
-    <a href={item.url}>
-      <img src={item.img} alt="" />
-    </a>
+    <img src={item.img} alt="" />
+    <div className="external">
+      <a href={item.demo} target="_blank" rel="noopener noreferrer">Live demo</a>
+      <a href={item.github} target="_blank" rel="noopener noreferrer">
+        <Github />
+        Source code
+      </a>
+    </div>
   </li>
 );
 
@@ -56,7 +74,8 @@ const Projects = () => (
 Item.propTypes = {
   item: PropTypes.shape({
     img: PropTypes.string,
-    url: PropTypes.string,
+    demo: PropTypes.string,
+    github: PropTypes.string,
   }).isRequired,
 };
 
